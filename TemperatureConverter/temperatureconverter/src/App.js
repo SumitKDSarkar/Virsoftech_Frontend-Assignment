5ee2494cff2fb4f1a606b052c9700c1350fcd74e
Sumit Kumar Dey Sarkar <skumarcbr4@gmail.com>
2023-09-07 16:21:43 +0530

    Add Temperature-Converter

---
+++ TemperatureConverter/temperatureconverter/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import TemperatureConverter from './Component/TemperatureConverter';
+
+function App() {
+  return (
+    <div className="App">
+      <TemperatureConverter/>
+    </div>
+  );
+}
+
+export default App;
---
+++ TemperatureConverter/temperatureconverter/src/Component/TemperatureConverter.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import './TemperatureConverter.css'; // Import the CSS file
+
+function TemperatureConverter() {
+  const [temperature, setTemperature] = useState('');
+  const [scale, setScale] = useState('Celsius');
+  const [result, setResult] = useState('');
+  const [error, setError] = useState('');
+
+  const handleTemperatureChange = (event) => {
+    setTemperature(event.target.value);
+    setResult('');
+    setError('');
+  };
+
+  const handleScaleChange = (event) => {
+    setScale(event.target.value);
+    setResult('');
+    setError('');
+  };
+
+  const convertToCelsius = () => {
+    if (!isNaN(temperature)) {
+      const celsius = scale === 'Celsius' ? parseFloat(temperature) : (parseFloat(temperature) - 32) * (5 / 9);
+      setResult(`${temperature}${scale} is equal to ${celsius.toFixed(2)}°C`);
+      setError('');
+    } else {
+      setError('Please enter a valid number');
+    }
+  };
+
+  const convertToFahrenheit = () => {
+    if (!isNaN(temperature)) {
+      const fahrenheit = scale === 'Fahrenheit' ? parseFloat(temperature) : (parseFloat(temperature) * (9 / 5)) + 32;
+      setResult(`${temperature}${scale} is equal to ${fahrenheit.toFixed(2)}°F`);
+      setError('');
+    } else {
+      setError('Please enter a valid number');
+    }
+  };
+
+  return (
+    <div className="temperature-converter">
+      <h1>Temperature Converter</h1>
+      <div className="input-container">
+        <input
+          type="text"
+          placeholder="Enter temperature"
+          value={temperature}
+          onChange={handleTemperatureChange}
+        />
+        <select value={scale} onChange={handleScaleChange}>
+          <option value="Celsius">Celsius</option>
+          <option value="Fahrenheit">Fahrenheit</option>
+        </select>
+      </div>
+      <div className="buttons-container">
+        <button onClick={convertToCelsius}>Convert to Celsius</button>
+        <button onClick={convertToFahrenheit}>Convert to Fahrenheit</button>
+      </div>
+      {error && <p className="error">{error}</p>}
+      {result && <p className="result">{result}</p>}
+    </div>
+  );
+}
+
+export default TemperatureConverter;
